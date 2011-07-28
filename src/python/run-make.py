d73588a9b1ac5dd1efad98fa9fb9be1aed02bde1
Roman Kashitsyn <roman.kashitsyn@gmail.com>
2011-07-28 16:45:23 +0400

    Add logging to run-make.py script.

--- src/python/run-make.py
+++ src/python/run-make.py
@@ -2,6 +2,7 @@
 import sys
 import os
 from subprocess import Popen, PIPE, STDOUT
+import logging
 from termcolor import colored
 import texlog
 
@@ -33,10 +34,17 @@ if __name__ == "__main__":
 
     if len(sys.argv) > 1:
         make_with_args.extend(sys.argv[1:])
+        logging.info("Running make with arguments: {0}", sys.argv[1:])
+    else:
+        logging.info("Running make without parameters")
 
     p = Popen(make_with_args, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
 
     stdoutdata, stderrdata = p.communicate()
+
+    if stderrdata != None:
+        logging.warning("Make produce error messages:")
+        print stderrdata
 
     if make_with_args.count('clean') > 0:
         exit()
